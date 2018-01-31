63d5e522767fa16456ee8d99d768cb12c0ebfa07
amite <amit.erandole@gmail.com>
2018-01-31 13:24:51 +0530

    rendering object list

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 
-const profileNames = ["munna", "poonam", "owais"]
+const profileNames = [
+  {
+    name: "poonam"
+  },
+  {
+    name: "munna"
+  },
+  {
+    name: "owais"
+  }
+]
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {profileNames.map((profile, index) => {
-          return <Text key={index}>{profile}</Text>
+        {profileNames.map(({ name }) => {
+          return <Text key={name}>{name}</Text>
         })}
       </View>
     )
