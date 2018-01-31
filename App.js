d115b4c75a1c5e4604c39c56801bdf70e4ca9cfc
amite <amit.erandole@gmail.com>
2018-01-31 13:22:23 +0530

    added simple text list

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { StyleSheet, Text, View } from "react-native"
 
+const profileNames = ["munna", "poonam", "owais"]
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>{"Open up App.js to start working on your app!"}</Text>
+        {profileNames.map((profile, index) => {
+          return <Text key={index}>{profile}</Text>
+        })}
       </View>
     )
   }
