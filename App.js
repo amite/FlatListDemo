5034b680f1f2f486b964dc0548975ca20caf7e8b
amite <amit.erandole@gmail.com>
2018-02-07 22:35:32 +0530

    adds scrollview and dynamic api data

--- App.js
+++ App.js
@@ -1,26 +1,37 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
-
-const profileNames = [
-  {
-    name: "poonam"
-  },
-  {
-    name: "munna"
-  },
-  {
-    name: "owais"
-  }
-]
+import { StyleSheet, Text, View, ScrollView } from "react-native"
 
 export default class App extends React.Component {
+  state = {
+    profiles: [],
+    loading: false
+  }
+
+  componentDidMount = async () => {
+    const url = `https://randomuser.me/api?results=60`
+    this.setState({ loading: true })
+
+    const res = await fetch(url)
+    const profiles = await res.json()
+
+    this.setState({
+      profiles: profiles.results,
+      loading: false
+    })
+  }
+
   render() {
+    const { profiles } = this.state
     return (
-      <View style={styles.container}>
-        {profileNames.map(({ name }) => {
-          return <Text key={name}>{name}</Text>
+      <ScrollView style={styles.container}>
+        {profiles.map(({ name: { first, last }, login: { username } }) => {
+          return (
+            <Text key={username} style={styles.item}>
+              {first} {last}
+            </Text>
+          )
         })}
-      </View>
+      </ScrollView>
     )
   }
 }
@@ -28,8 +39,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    backgroundColor: "steelblue",
+    paddingTop: 50,
+    padding: 30
+  },
+  item: {
+    marginBottom: 10
   }
 })
